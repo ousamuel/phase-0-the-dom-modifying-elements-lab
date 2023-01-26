c989eeac3cf761493544e43616e1b36abcf6506e
Samuel <samuelou510@gmail.com>
2023-01-26 15:11:58 -0500

    commit tree d28e26e1aa4c1cdd708ba0e0016a1793661720f4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+var element = document.createElement("h1");
+const newHeader = document.getElementsByName("h1");
+newHeader.nodeName = "H1";
+newHeader.id = "victory";
+newHeader.innerHTML = "is the champion";
